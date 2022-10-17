04356b22172d07425b3ffa7f57ec6cf52a8c546c
Valerija <115976329+vbizomec@users.noreply.github.com>
2022-10-17 14:51:58 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
        Person firstPerson = new Person("Valerija Bizomec", "Croatia");
         System.out.println(firstPerson.getName());
+        System.out.println(firstPerson.getCountry());
     }
 }
