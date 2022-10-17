54b68709685fce44f74130991e07adf007091fef
Valerija Bizomec <vbizomec@gmail.com>
2022-10-17 14:36:36 +0200

    My second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
        Person firstPerson = new Person("Valerija Bizomec", "Croatia");
+        System.out.println(firstPerson.getName());
     }
 }
